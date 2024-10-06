b9b9fd810f83a4536d029e97e58dd052cfc536a9
naveed <naveedinno@proton.me>
2024-10-06 15:11:52 +0330

    Use hash for detecting duplicate symbols instead of name

--- helper_contracts/contracts/test/MockSymmio.sol
+++ helper_contracts/contracts/test/MockSymmio.sol
@@ -4,59 +4,52 @@ pragma solidity ^0.8.18;
 import "../interfaces/ISymmio.sol";
 
 contract MockSymmio is ISymmio {
-    mapping(address => bool) public partyBEmergencyStatus;
-    mapping(uint256 => Quote) public mockQuotes;
-    mapping(uint256 => Symbol) public mockSymbols;
+	mapping(address => bool) public partyBEmergencyStatus;
+	mapping(uint256 => Quote) public mockQuotes;
+	mapping(uint256 => Symbol) public mockSymbols;
+	Symbol[] symbols;
 
-    event PartyBEmergencyStatusSet(address partyB, bool status);
+	event PartyBEmergencyStatusSet(address partyB, bool status);
 
-    function setPartyBEmergencyStatus(
-        address[] memory partyBs,
-        bool status
-    ) external {
-        for (uint i = 0; i < partyBs.length; i++) {
-            partyBEmergencyStatus[partyBs[i]] = status;
-            emit PartyBEmergencyStatusSet(partyBs[i], status);
-        }
-    }
+	function setPartyBEmergencyStatus(address[] memory partyBs, bool status) external {
+		for (uint i = 0; i < partyBs.length; i++) {
+			partyBEmergencyStatus[partyBs[i]] = status;
+			emit PartyBEmergencyStatusSet(partyBs[i], status);
+		}
+	}
 
-    function getPartyBEmergencyStatus(
-        address partyB
-    ) external view returns (bool) {
-        return partyBEmergencyStatus[partyB];
-    }
+	function getPartyBEmergencyStatus(address partyB) external view returns (bool) {
+		return partyBEmergencyStatus[partyB];
+	}
 
-    function setMockQuote(uint256 quoteId, Quote memory quote) public {
-        mockQuotes[quoteId] = quote;
-    }
+	function setMockQuote(uint256 quoteId, Quote memory quote) public {
+		mockQuotes[quoteId] = quote;
+	}
 
-    function getQuote(uint256 quoteId) public view returns (Quote memory) {
-        return mockQuotes[quoteId];
-    }
+	function getQuote(uint256 quoteId) public view returns (Quote memory) {
+		return mockQuotes[quoteId];
+	}
 
-    function setMockSymbol(uint256 symbolId, Symbol memory symbol) public {
-        mockSymbols[symbolId] = symbol;
-    }
+	function setMockSymbol(uint256 symbolId, Symbol memory symbol) public {
+		mockSymbols[symbolId] = symbol;
+		symbols.push(symbol);
+	}
 
-    function getSymbol(uint256 symbolId) public view returns (Symbol memory) {
-        return mockSymbols[symbolId];
-    }
+	function getSymbol(uint256 symbolId) public view returns (Symbol memory) {
+		return mockSymbols[symbolId];
+	}
 
-    function setSymbolTradingFee(
-        uint256 symbolId,
-        uint256 tradingFee
-    ) external {}
+	function getSymbols(uint256 start, uint256 size) public view returns (Symbol[] memory) {
+		return symbols;
+	}
 
-    function setForceCloseGapRatio(
-        uint256 symbolId,
-        uint256 forceCloseGapRatio
-    ) external {}
+	function setSymbolTradingFee(uint256 symbolId, uint256 tradingFee) external {}
 
-    function forceCloseGapRatio(
-        uint256 symbolId
-    ) external view returns (uint256) {
-        return 1;
-    }
+	function setForceCloseGapRatio(uint256 symbolId, uint256 forceCloseGapRatio) external {}
 
-    function addSymbols(Symbol[] memory symbols) external override {}
+	function forceCloseGapRatio(uint256 symbolId) external view returns (uint256) {
+		return 1;
+	}
+
+	function addSymbols(Symbol[] memory symbols) external override {}
 }
